1348427e003b097625dd80fc682621c8c8fe308e
shenyang <shenyang>
2018-05-11 10:25:46 +0800

    Initial Commit

---
+++ TicketObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicketObject
+//
+//  Created by 沈阳 on 2018/5/11.
+//  Copyright © 2018年 沈阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicketObject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicketObject
+//
+//  Created by 沈阳 on 2018/5/11.
+//  Copyright © 2018年 沈阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
